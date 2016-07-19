e9569726c9c2d90df5f58c3f9cfc1b3b34b35682
Alex <night_guard@abv.bg>
2016-07-20 01:53:07 +0300

    Externalized the divine messages into separate resource files. Praise be to Om!

--- src/main/java/chat/websocket/Responses.java
+++ src/main/java/chat/websocket/Responses.java
@@ -1,32 +1,68 @@
 package chat.websocket;
 
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 
 public class Responses
 {
-    private static String[] welcomeMessages = new String[]{"Welcome, my child!",
-                                                           "Pray, and your prayer will be answered!",
-                                                           "You have my godly attention.",
-                                                           "I will not hear your prayers, lest you offer some lettuce first."};
+    private static List<String> welcomeMessages;
 
-    private static String[] sillyResponses = new String[]{
-                                                          "No prayers will be answered until I get some lettuce!",
-                                                          "Very well, my godly persona finds your request ... pleasing, and will fulfil it!",
-                                                          "I don't quite understand what you need, but everything will be alright in the end.",
-                                                          "Hmpf... who were you, again?", 
-                                                          "Your prayer has been heard, my child, and will be executed in due time. Prayers that have to be answered before it: 42442"};
+    private static List<String> genericResponses;
+
+    static
+    {
+        welcomeMessages = new ArrayList<String>();
+        genericResponses = new ArrayList<String>();
+//        try
+//        {
+//            readFileIntoList(welcomeMessages, "private/welcomeMessages.txt");
+//            readFileIntoList(genericResponses, "private/genericResponses.txt");
+//        }
+//        catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+    }
 
 
     public static String getWelcomeMessage()
     {
-        return welcomeMessages[new Random().nextInt(welcomeMessages.length)];
+        return welcomeMessages.get(new Random().nextInt(welcomeMessages.size()));
     }
 
 
     public static String getPrayerAnswer(String message)
     {
-        return sillyResponses[new Random().nextInt(sillyResponses.length)];
+        return genericResponses.get(new Random().nextInt(genericResponses.size()));
+    }
+
+
+    private static void readFileIntoList(List<String> list, String file)
+        throws IOException
+    {
+        try (InputStream is = Responses.class.getClassLoader().getResourceAsStream(file))
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while ((line = br.readLine()) != null)
+            {
+                line = line.trim();
+                list.add(line);
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws IOException
+    {
+        readFileIntoList(genericResponses, "src/main/resources/private/genericResponses.txt");
+        System.out.println(genericResponses.size());
     }
 }
